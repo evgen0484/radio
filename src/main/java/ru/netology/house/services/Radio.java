fbefce4ed86e420b4dd249bf65fa8876124c0a58
Evgeniy Kaliuzhniy <ewgeniykaliuzhniy202@gmail.com>
2022-10-20 19:14:34 +0500

    src/Main.java

---
+++ src/main/java/ru/netology/house/services/Radio.java
@@ -0,0 +1,63 @@
+package ru.netology.house.services;
+
+public class Radio {
+    private int currentStation;
+    private int currentVolume;
+
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation < 0) {
+            return;
+        }
+        if (currentStation > 9) {
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public void nextStation() {
+        if (9 <= currentStation) {
+            setCurrentStation(0);
+        } else {
+            setCurrentStation(currentStation + 1);
+        }
+    }
+
+    public void previousStation() {
+        if (currentStation <= 0) {
+            setCurrentStation(9);
+        } else {
+            setCurrentStation(currentStation - 1);
+        }
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume < 0) {
+            return;
+        }
+        if (currentVolume > 10) {
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < 10) {
+            currentVolume++;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume > 0) {
+            currentVolume--;
+        }
+    }
+}
